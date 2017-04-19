40c2d2f0a7267cbb1c0d1074ebc68ffe31754b65
mahfuzuddin <mahfuzuddin347@gmail.com>
2017-04-19 22:20:36 +0000

    new new

--- subway.js
+++ subway.js
@@ -1,29 +1,58 @@
 // console.log('is jquery here?', $);
+$(document).ready(function() {
+      $.ajax({
+        url: "https://data.cityofnewyork.us/resource/kk4q-3rt2.json",
+        type: "GET",
+        data: {
+          "$limit": 5000,
+          "$$app_token": "GFM7yJOMboJWrRvx7JCKefRKb"
+        }
+      }).done(function(data) {
+        //   alert("Retrieved " + data.length + " records from the dataset!");
+        window.mydata = data;
+        var line = "0";
+        var name = "0";
+        console.log("before loop")
+        for (var i = 0; i < window.mydata.length; i++) {
+          var line = window.mydata[i].line
+          console.log("line is");
+          console.log(line);
+        }
 
-$.ajax({
-    url: "https://data.cityofnewyork.us/resource/kk4q-3rt2.json",
-    type: "GET",
-    data: {
-      "$limit" : 5000,
-      "$$app_token" : "GFM7yJOMboJWrRvx7JCKefRKb"
-    }
-}).done(function(data) {
-//   alert("Retrieved " + data.length + " records from the dataset!");
-  window.mydata= data;
-  var line= "0";
-  var name= "0";
-  console.log("before loop")
-  for(var i=0; i<window.mydata.length;i++ ){
-    var line = window.mydata[i].line
-    console.log("line is");
-    console.log(line);
-  }
-  console.log("after loop")
-  var results = ["One","Two","Three","Four","Five","Six"];
-  var list = "";
-  for(var i=0; i<results.length; i++) {
-    list += "<li>" + results[i] + "</li>";
-  }
-   $('.results').append(list);
-});
+        for (var i = 0; i < window.mydata.length; i++) {
+          var name = window.mydata[i].name
+          console.log("station is");
+          console.log(name);
+        }
 
+
+        console.log("after loop")
+        var results = ["One", "Two", "Three", "Four", "Five", "Six"];
+        var list = "";
+        for (var i = 0; i < results.length; i++) {
+          list += "<li>" + results[i] + "</li>";
+        }
+        $('.results').append(list);
+        
+        var trains = ["1","2","3","9","4","5","6"]
+        for (var i=0; i < trains.length; i++) {
+          console.log("trains i " + trains[i])
+        
+          var circleHtml =
+            '<a class="btn red" id="first" href="#">' +
+              '<div class="circle">' + trains[i] + '</div>' +
+                '<i class="ion-ios-arrow-down">' +  
+              '</i>' +
+            '</a>';
+          $("#all").append(circleHtml);
+        }
+        
+         $(".btn").click(function() {
+          alert("button was clicked");
+        })
+      });
+      
+       
+
+})
+      
